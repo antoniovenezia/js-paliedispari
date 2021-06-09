fee0401b9d51c7e9d1bdd4241318178d3b3549c0
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-09 14:58:50 +0200

    js-paliedispari

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // PALINDROME TEST
-var wordUser = prompt("Inserisci una parola!");
-var palindromTest = document.getElementById("test");
+var wordUser = prompt("Inserisci una parola PALINDROMA!");
+var test = document.getElementById("test");
 
 function palindrome(str) {
     var reversed = str.split("").reverse().join("");
@@ -8,5 +8,38 @@ function palindrome(str) {
     return false;
 }
 
-palindromTest.innerHTML = "La parola inserita è Palindroma? " + palindrome(wordUser);
+test.innerHTML = "La parola inserita è Palindroma? " + palindrome(wordUser);
+
+
+
+// PARI E DISPARI
+var numberUser = prompt("Scegli un numero da 1 a 5");
+
+function num(min, max){
+    var result = Math.floor(Math.random() * (max - min + 1) + min);
+    return result;
+}
+
+var numPc = num(1, 5);
+console.log(numPc);
+
+var resultSum = parseInt(numberUser) + parseInt(numPc);
+console.log(resultSum);
+
+
+function isPari(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (isPari(resultSum)) {
+    test.innerHTML += "\nL'utente ha vinto";
+} else {
+    test.innerHTML += "\nL'utente ha perso";
+}
+
+
 
