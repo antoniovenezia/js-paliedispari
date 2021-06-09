e04710a824a32d8f51bf9906f7fff4d6068cd655
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-09 16:20:23 +0200

    js-paliedispari

--- js/script.js
+++ js/script.js
@@ -13,32 +13,48 @@ test.innerHTML = "La parola inserita è Palindroma? " + palindrome(wordUser);
 
 
 // PARI E DISPARI
-var numberUser = prompt("Scegli un numero da 1 a 5");
 
-function num(min, max){
+// Funzioni
+function num(min, max) {
     var result = Math.floor(Math.random() * (max - min + 1) + min);
     return result;
 }
 
+function isPari(num) {
+    if (num % 2 === 0) {
+        return "pari";
+        
+    } else {
+        return "dispari";
+    }
+}
+
+
+
+// Variabili
+var wordUser = prompt("Pari o Dispari?");
+console.log(wordUser);
+
+
+var numberUser = prompt("Scegli un numero da 1 a 5");
+console.log(numberUser);
+
+
 var numPc = num(1, 5);
 console.log(numPc);
+
 
 var resultSum = parseInt(numberUser) + parseInt(numPc);
 console.log(resultSum);
 
 
-function isPari(num) {
-    if (num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
-if (isPari(resultSum)) {
-    test.innerHTML += "\nL'utente ha vinto";
+
+// IF
+if (isPari(resultSum) && wordUser === "Pari") {
+    test.innerHTML += "\nNell'esercizio del Pari o Dispari l'utente ha vinto";
 } else {
-    test.innerHTML += "\nL'utente ha perso";
+    test.innerHTML += "\nNell'esercizio del Pari o Dispari l'utente ha perso";
 }
 
 
